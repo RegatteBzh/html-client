9ac29d66d4f9b28656f21b03e93ab5267d92d082
Cyrille Meichel <cmeichel@free.fr>
2017-08-19 11:40:20 +0200

    bind compass

--- src/app/compass/compass.component.ts
+++ src/app/compass/compass.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-compass',
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompassComponent implements OnInit {
 
-  constructor() { }
+  private directionValue = 0;
+  private mouseDownOnCursor: boolean;
+
+  @Output() directionChange = new EventEmitter();
+  @Input() get direction() {
+    return this.directionValue;
+  }
+
+  set direction(val) {
+    this.directionValue = val;
+    this.directionChange.emit(this.directionValue);
+  }
+
+  constructor() {
+    this.mouseDownOnCursor = false;
+  }
+
+  getAngle(x: number, y: number) {
+    if (x === 0 && y === 0) {
+      return 0;
+    }
+    if (x === 0 && y > 0) {
+      return 0;
+    }
+    if (x === 0 && y < 0) {
+      return -180;
+    }
+    if (x > 0) {
+      return 90 - Math.atan(y / x) * 180 / Math.PI;
+    }
+    if (y < 0 && x > 0) {
+      return 90 + Math.atan(y / x) * 180 / Math.PI;
+    }
+    if (y < 0 && x < 0) {
+      return 270 - Math.atan(y / x) * 180 / Math.PI;
+    }
+    if (y > 0 && x < 0) {
+      return 270 - Math.atan(y / x) * 180 / Math.PI;
+    }
+    return 180 + Math.atan(y / x) * 180 / Math.PI;
+  }
 
   ngOnInit() {
+  }
+
+  mouseCursorDown() {
+    this.mouseDownOnCursor = true;
+  }
+
+  mouseCursorUp() {
+    this.mouseDownOnCursor = false;
+  }
+
+  mouseCursorMove(event) {
+    if (this.mouseDownOnCursor) {
+      const x = event.offsetX - 120;
+      const y = 120 - event.offsetY;
+      this.direction = Math.round(this.getAngle(x, y));
+    }
+  }
+
+  getRotation() {
+    return `rotate(${this.direction})`;
   }
 
 }
