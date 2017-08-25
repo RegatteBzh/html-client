d0fca834d06f4ef2d680490b3962dd94be6ade34
Cyrille Meichel <cmeichel@free.fr>
2017-08-26 00:42:14 +0200

    more maps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -19,34 +19,62 @@ export class MapComponent implements OnInit {
   public zoom = 2;
   public maxBound: LatLngBounds;
 
+  public currentMap;
+
   public maps = [
     {
-      name: 'topo',
-      url: 'https://b.tile.opentopomap.org/{z}/{x}/{y}.png',
-      opacity: 1,
-      selected: true
+      name: 'Topo',
+      url: 'https://b.tile.opentopomap.org/{z}/{x}/{y}.png'
     },
     {
-      name: 'basic',
-      url: 'http://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      opacity: 0
+      name: 'Basic',
+      url: 'http://b.tile.openstreetmap.org/{z}/{x}/{y}.png'
     },
     {
-      name: 'etopo',
-      url: 'https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}',
-      opacity: 0
+      name: 'Etopo ocean',
+      url: 'https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}'
+    },
+    {
+      name: 'Etopo streets',
+      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}.png'
+    },
+    {
+      name: 'Etopo light gray',
+      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+    },
+    {
+      name: 'Etopo dark gray',
+      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+    },
+    {
+      name: 'Etopo national-geographics',
+      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
+    },
+    {
+      name: 'Etopo world imagery',
+      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+    },
+    {
+      name: 'Thunder forest a',
+      url: 'https://a.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d'
+    },
+    {
+      name: 'Thunder forest b',
+      url: 'https://b.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d'
+    },
+    {
+      name: 'Thunder forest c',
+      url: 'https://c.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d'
     }
   ];
 
   constructor() {
     this. maxBound = new LatLngBounds(new LatLng(-90, -180), new LatLng(90, 180));
+    this.currentMap = this.maps[0];
   }
 
   onSelectionChange(map) {
-    this.maps.forEach(mapElt => {
-      mapElt.opacity = 0;
-    });
-    map.opacity = 1;
+    this.currentMap = map;
   }
 
   ngOnInit() {
