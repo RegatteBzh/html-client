d41f11d5684835c8a2f694b0462939ae3f5b8972
Cyrille Meichel <cmeichel@free.fr>
2017-11-29 22:36:25 +0100

    display log fields

--- src/app/models/graylog-message.ts
+++ src/app/models/graylog-message.ts
@@ -1,11 +1,16 @@
 export class GraylogMessage {
     public _id: string;
-    public message: string;
+    public msg: string;
     public level: number;
     public timestamp: Date;
     public source: string;
     public title: string;
     public full: string;
+    public ip: string;
+    public latency: string;
+    public method: string;
+    public path: string;
+
 
     constructor (data: any) {
         data.timestamp = new Date(data.timestamp);
