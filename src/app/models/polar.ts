fe4ca193f48092355dd1e85ee32b49ed2ee2b9f3
Cyrille Meichel <cmeichel@free.fr>
2017-11-30 19:07:07 +0100

    better interpolation of the polar

--- src/app/models/polar.ts
+++ src/app/models/polar.ts
@@ -10,21 +10,44 @@ export class Polar {
     }
 
     getSpeedCoefAt(wind: number, direction: number): number {
-        const sampledWind0 = Math.floor(wind / 10);
-        const sampledWind1 = sampledWind0 + 1;
-        const sampledDirection = Math.round((direction % 180) / 5);
-        if (this.data.length > sampledWind0) {
-            if (this.data.length > sampledWind1) {
-                const coef = (wind - Math.floor(wind / 10) * 10) / 10;
-                const interp1 = this.data[sampledWind1][sampledDirection];
-                const interp0 = this.data[sampledWind0][sampledDirection];
-                return interp0 + (interp1 - interp0) * coef;
-            }
-
-            return this.data[sampledWind0][sampledDirection];
-        } else {
-            return this.data[this.data.length - 1][sampledDirection];
+        let sampledWind0 = Math.floor(wind / 10);
+        if (this.data.length < sampledWind0 + 1) {
+            sampledWind0 = this.data.length - 1;
         }
+        let sampledWind1 = sampledWind0 + 1;
+        if (this.data.length < sampledWind1 + 1) {
+            sampledWind1 = sampledWind0;
+        }
+        const sampledDirection0 = Math.floor(direction / 5) % 180;
+        const sampledDirection1 = Math.ceil((direction + 5) / 5) % 180;
+        return this.interpolation(
+            wind / 10 - Math.floor(wind / 10),
+            (direction % 180) / 5 - sampledDirection0,
+            this.data[sampledWind0][sampledDirection0],
+            this.data[sampledWind1][sampledDirection0],
+            this.data[sampledWind0][sampledDirection1],
+            this.data[sampledWind1][sampledDirection1],
+        );
+    }
+
+     /**
+     * Interpolate value
+     * @param x variation between g0* and g1*
+     * @param y variation between g*0 dans g*1
+     * @param g00 point at col_0 and line_0
+     * @param g10 point at col_1 and line_0
+     * @param g01 point at col_0 and line_1
+     * @param g11 point at col_1 and line_1
+     * @return interpolated vector
+     */
+    interpolation (x: number, y: number, g00: number, g10: number, g01: number, g11: number): number {
+        const rx = (1 - x);
+        const ry = (1 - y);
+        const a = rx * ry;
+        const b = x * ry;
+        const c = rx * y;
+        const d = x * y;
+        return g00 * a + g10 * b + g01 * c + g11 * d;
     }
 
 }
